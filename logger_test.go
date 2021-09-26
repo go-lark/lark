d3ba6178e7276de602b62b29b86f90e2ac5ee665
David Zhang <crispgm@gmail.com>
2021-09-26 18:01:31 +0800

    fix: more test coverage

--- logger_test.go
+++ logger_test.go
@@ -17,4 +17,14 @@ func TestSetLogger(t *testing.T) {
 func TestLogLevel(t *testing.T) {
 	var logLevel LogLevel = LogLevelDebug
 	assert.Equal(t, "DEBUG", logLevel.String())
+	logLevel = LogLevelError
+	assert.Equal(t, "ERROR", logLevel.String())
+	logLevel = LogLevelTrace
+	assert.Equal(t, "TRACE", logLevel.String())
+	logLevel = LogLevelWarn
+	assert.Equal(t, "WARN", logLevel.String())
+	logLevel = LogLevelInfo
+	assert.Equal(t, "INFO", logLevel.String())
+	logLevel = 1000
+	assert.Equal(t, "", logLevel.String())
 }
