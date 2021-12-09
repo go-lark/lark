198c3541162f68f44fd123293ed645710828f8a6
David Zhang <crispgm@gmail.com>
2021-12-09 21:25:33 +0800

    fix: support card

--- api_notification_test.go
+++ api_notification_test.go
@@ -81,7 +81,7 @@ func TestWebhookV2CardMessage(t *testing.T) {
 			AddImage(b.Img("img_a7c6aa35-382a-48ad-839d-d0182a69b4dg")),
 	).
 		Wathet().
-		Title("卡片标题 Card Title")
+		Title("Notification Card")
 	msgV4 := NewMsgBuffer(MsgInteractive)
 	omV4 := msgV4.Card(card.String()).Build()
 	resp, err := bot.PostNotificationV2(omV4)
@@ -89,52 +89,5 @@ func TestWebhookV2CardMessage(t *testing.T) {
 	if assert.NoError(t, err) {
 		assert.Equal(t, 0, resp.StatusCode)
 		assert.NotEmpty(t, resp.StatusMessage)
-	}
-}
-
-func TestWebhookV2Errors(t *testing.T) {
-	bot := NewNotificationBot(ciHookURLV2)
-
-	resp, err := bot.PostNotification("go-lark CI", "it works")
-	assert.NoError(t, err)
-	assert.False(t, resp.Ok)
-
-	msg := NewMsgBuffer(MsgInteractive)
-	respV2, err := bot.PostNotificationV2(msg.Build())
-	assert.Nil(t, respV2)
-	assert.Error(t, ErrMessageTypeNotSuppored, err)
-}
-
-func TestWebhookV2InteractiveErrors(t *testing.T) {
-	bot := NewNotificationBot(ciHookURLV2)
-
-	b := NewCardBuilder()
-	card := b.Card(
-		b.Action(
-			b.Button(b.Text("**Primary**").LarkMd()).Primary(),
-			b.Button(b.Text("Confirm")).Confirm("Confirm", "Are you sure?"),
-			b.Overflow(
-				b.Option("Option 1"),
-				b.Option("选项2"),
-			).Value(map[string]interface{}{"k": "v"}),
-		).TrisectionLayout(),
-		b.Action(
-			b.SelectMenu(
-				b.Option("Option 1"),
-				b.Option("选项2"),
-			).
-				Placeholder("select").
-				Value(map[string]interface{}{"k": "v"}),
-		),
-	).
-		Wathet().
-		Title("卡片标题 Card Title")
-	msgV4 := NewMsgBuffer(MsgInteractive)
-	omV4 := msgV4.Card(card.String()).Build()
-	resp, err := bot.PostNotificationV2(omV4)
-
-	if assert.NoError(t, err) {
-		assert.Equal(t, 0, resp.StatusCode)
-		assert.Empty(t, resp.StatusMessage)
 	}
 }
