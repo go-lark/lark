cb991eb54276234d236a5bbc14061710b57a6790
machinly <machinly@acm.org>
2021-12-13 15:38:50 +0800

    feat: embed BaseResponse for more error info (#22)

--- api_notification.go
+++ api_notification.go
@@ -7,6 +7,7 @@ type PostNotificationResp struct {
 
 // PostNotificationV2Resp response of PostNotificationV2
 type PostNotificationV2Resp struct {
+	BaseResponse
 	StatusCode    int    `json:"StatusCode"`
 	StatusMessage string `json:"StatusMessage"`
 }
