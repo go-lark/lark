9e323ccc75cdf8fadb6ab53bd95bf036ac2bf03d
David Zhang <crispgm@gmail.com>
2021-12-14 11:31:35 +0800

    refactor: avoid *&

--- message_v1.go
+++ message_v1.go
@@ -13,13 +13,13 @@ func BuildOutcomingMessageReq(om OutcomingMessage) map[string]interface{} {
 	}
 	content := make(map[string]interface{})
 	if om.Content.Text != nil {
-		content["text"] = *&om.Content.Text.Text
+		content["text"] = om.Content.Text.Text
 	}
 	if om.Content.Image != nil {
-		content["image_key"] = *&om.Content.Image.ImageKey
+		content["image_key"] = om.Content.Image.ImageKey
 	}
 	if om.Content.ShareChat != nil {
-		content["share_open_chat_id"] = *&om.Content.ShareChat.ChatID
+		content["share_open_chat_id"] = om.Content.ShareChat.ChatID
 	}
 	if om.Content.Post != nil {
 		content["post"] = *om.Content.Post
