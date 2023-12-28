47b1ca6d0a779a1b507bd69306c07738c45f4868
David Zhang <crispgm@gmail.com>
2023-12-28 11:35:21 +0800

    feat(contact): support user info (#67)

--- error.go
+++ error.go
@@ -7,6 +7,7 @@ var (
 	ErrBotTypeError           = errors.New("Bot type error")
 	ErrParamUserID            = errors.New("Param error: UserID")
 	ErrParamMessageID         = errors.New("Param error: Message ID")
+	ErrParamExceedInputLimit  = errors.New("Param error: Exceed input limit")
 	ErrMessageTypeNotSuppored = errors.New("Message type not supported")
 	ErrEncryptionNotEnabled   = errors.New("Encryption is not enabled")
 	ErrCustomHTTPClientNotSet = errors.New("Custom HTTP client not set")
