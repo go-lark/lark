b86d9acc36602ddf9defe3a23161da2fb835841a
David Zhang <crispgm@gmail.com>
2025-05-24 17:41:55 +0800

    chore: rename events

--- v2/event.go
+++ v2/event.go
@@ -1,38 +1,93 @@
 package lark
 
-import "encoding/json"
+import (
+	"bytes"
+	"encoding/json"
+	"log"
+	"net/http"
+)
 
-// EventChallenge request of add event hook
-type EventChallenge struct {
-	Token     string `json:"token,omitempty"`
-	Challenge string `json:"challenge,omitempty"`
-	Type      string `json:"type,omitempty"`
+// EventType definitions
+const (
+	EventTypeMessageReceived        = "im.message.receive_v1"
+	EventTypeMessageRead            = "im.message.message_read_v1"
+	EventTypeMessageRecalled        = "im.message.recalled_v1"
+	EventTypeMessageReactionCreated = "im.message.reaction.created_v1"
+	EventTypeMessageReactionDeleted = "im.message.reaction.deleted_v1"
+	EventTypeChatDisbanded          = "im.chat.disbanded_v1"
+	EventTypeUserAdded              = "im.chat.member.user.added_v1"
+	EventTypeUserDeleted            = "im.chat.member.user.deleted_v1"
+	EventTypeBotAdded               = "im.chat.member.bot.added_v1"
+	EventTypeBotDeleted             = "im.chat.member.bot.deleted_v1"
+	// not supported yet
+	EventTypeChatUpdated   = "im.chat.updated_v1"
+	EventTypeUserWithdrawn = "im.chat.member.user.withdrawn_v1"
+)
+
+// Event handles events with v2 schema
+type Event struct {
+	Schema string      `json:"schema,omitempty"`
+	Header EventHeader `json:"header,omitempty"`
+
+	EventRaw json.RawMessage `json:"event,omitempty"`
+	Event    interface{}     `json:"-"`
 }
 
-// EventChallengeReq is deprecated. Keep for legacy versions.
-type EventChallengeReq = EventChallenge
-
-// EncryptedReq request of encrypted challenge
-type EncryptedReq struct {
-	Encrypt string `json:"encrypt,omitempty"`
+// EventHeader .
+type EventHeader struct {
+	EventID    string `json:"event_id,omitempty"`
+	EventType  string `json:"event_type,omitempty"`
+	CreateTime string `json:"create_time,omitempty"`
+	Token      string `json:"token,omitempty"`
+	AppID      string `json:"app_id,omitempty"`
+	TenantKey  string `json:"tenant_key,omitempty"`
 }
 
-// EventCardCallback request of card
-type EventCardCallback struct {
-	AppID     string          `json:"app_id,omitempty"`
-	TenantKey string          `json:"tenant_key,omitempty"`
-	Token     string          `json:"token,omitempty"`
-	OpenID    string          `json:"open_id,omitempty"`
-	UserID    string          `json:"user_id,omitempty"`
-	MessageID string          `json:"open_message_id,omitempty"`
-	ChatID    string          `json:"open_chat_id,omitempty"`
-	Action    EventCardAction `json:"action,omitempty"`
+// EventBody .
+type EventBody struct {
+	Type          string `json:"type"`
+	AppID         string `json:"app_id"`
+	TenantKey     string `json:"tenant_key"`
+	ChatType      string `json:"chat_type"`
+	MsgType       string `json:"msg_type"`
+	RootID        string `json:"root_id,omitempty"`
+	ParentID      string `json:"parent_id,omitempty"`
+	OpenID        string `json:"open_id,omitempty"`
+	OpenChatID    string `json:"open_chat_id,omitempty"`
+	OpenMessageID string `json:"open_message_id,omitempty"`
+	IsMention     bool   `json:"is_mention,omitempty"`
+	Title         string `json:"title,omitempty"`
+	Text          string `json:"text,omitempty"`
+	RealText      string `json:"text_without_at_bot,omitempty"`
+	ImageKey      string `json:"image_key,omitempty"`
+	ImageURL      string `json:"image_url,omitempty"`
+	FileKey       string `json:"file_key,omitempty"`
 }
 
-// EventCardAction .
-type EventCardAction struct {
-	Tag      string          `json:"tag,omitempty"`      // button, overflow, select_static, select_person, &datepicker
-	Option   string          `json:"option,omitempty"`   // only for Overflow and SelectMenu
-	Timezone string          `json:"timezone,omitempty"` // only for DatePicker
-	Value    json.RawMessage `json:"value,omitempty"`    // for any elements with value
+// EventUserID .
+type EventUserID struct {
+	UnionID string `json:"union_id,omitempty"`
+	UserID  string `json:"user_id,omitempty"`
+	OpenID  string `json:"open_id,omitempty"`
+}
+
+// PostEvent posts events
+func (e Event) PostEvent(client *http.Client, hookURL string) (*http.Response, error) {
+	buf := new(bytes.Buffer)
+	err := json.NewEncoder(buf).Encode(e)
+	if err != nil {
+		log.Printf("Encode json failed: %+v\n", err)
+		return nil, err
+	}
+	resp, err := client.Post(hookURL, "application/json; charset=utf-8", buf)
+	return resp, err
+}
+
+// GetEvent .
+func (e Event) GetEvent(eventType string, body interface{}) error {
+	if e.Header.EventType != eventType {
+		return ErrEventTypeNotMatch
+	}
+	err := json.Unmarshal(e.EventRaw, &body)
+	return err
 }
