c231aca976501a5e7aad161bb0e31f72a854e1a9
David Zhang <crispgm@gmail.com>
2025-05-30 16:36:07 +0800

    ci: setup multiple ci for v1 and v2 [ci skip]

---
+++ scripts/test_v1.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+echo "mode: atomic" >coverage.txt
+
+go test -coverprofile=profile.out -covermode=atomic ./...
+if [ -f profile.out ]; then
+	tail -q -n +2 profile.out >>coverage.txt
+	rm profile.out
+fi
+
+go tool cover -func coverage.txt
--- scripts/test_v2.sh
+++ scripts/test_v2.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 set -e
-echo "mode: atomic" > coverage.txt
+echo "mode: atomic" >coverage.txt
 
+cd ./v2
 go test -coverprofile=profile.out -covermode=atomic ./...
 if [ -f profile.out ]; then
-    tail -q -n +2 profile.out >> coverage.txt
-    rm profile.out
+	tail -q -n +2 profile.out >>coverage.txt
+	rm profile.out
 fi
 
 go tool cover -func coverage.txt
